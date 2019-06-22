28cfc2f9450ccc7f423144da6bf335300878bf51
Nayniv <44341420+Nayniv@users.noreply.github.com>
2019-06-22 13:44:10 +0300

    Выезд формы

---
+++ js/script.js
@@ -0,0 +1,6 @@
+var buttonSearch = document.querySelector(".button-search");
+var formSearch = document.querySelector(".search-form");
+
+    buttonSearch.addEventListener("click", function() {
+      formSearch.classList.toggle("searh-form-hidden");
+    });
