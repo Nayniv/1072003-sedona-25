a02a7871e77caee5575f5eb6ad7d577a906ccdf9
Nayniv <44341420+Nayniv@users.noreply.github.com>
2019-06-24 11:01:23 +0300

    обработка незаполненных полей

--- js/script.js
+++ js/script.js
@@ -1,6 +1,26 @@
 var buttonSearch = document.querySelector(".button-search");
 var formSearch = document.querySelector(".search-form");
+var arrival = formSearch.querySelector("[name=arrival]");
+var departure = formSearch.querySelector("[name=departure]");
+var adults = formSearch.querySelector("[name=adults]");
+var children = formSearch.querySelector("[name=children]");
 
-    buttonSearch.addEventListener("click", function() {
-      formSearch.classList.toggle("searh-form-hidden");
-    });
+
+
+
+buttonSearch.addEventListener("click", function() {
+  formSearch.classList.toggle("search-form-hidden");
+});
+
+formSearch.addEventListener('transitionend', function() {
+  if (!formSearch.classList.contains('searh-form-hidden')) arrival.focus();
+});
+
+formSearch.addEventListener("submit", function(evt) {
+  if (!arrival.value || !departure.value || !adults.value || !children.value) {
+    evt.preventDefault();
+    formSearch.classList.remove("form-error");
+    formSearch.offsetWidth = formSearch.offsetWidth;
+    formSearch.classList.add("form-error");
+  }
+});
